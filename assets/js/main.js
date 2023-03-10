2522e8938cef0d8d0673eb96ff5c146a54e6a8b4
CarlosFortunatoDev <carlosfortunatodev@gmail.com>
2023-03-10 01:23:27 -0300

    Adicionando JavaScrip para manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-
-
-
 const offset = 0;
 const limit = 10;
 
@@ -12,45 +9,20 @@ function convertPokemonToLi(pokemon) {
     return '<li class="pokemon"> <span class="number">#001</span> <span class="name">'+pokemon.name+'</span> <div class="detail"> <ol class="types"> <li class="type">grass</li> <li class="type">poison</li> </ol> <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="'+pokemon.name+'"> </div> </li>'
 }
 
-console.log(document.getElementById('pokemonList'))
+const pokemonList = document.getElementById('pokemonList')
 
 
 
-
-
-
-
-
-
-// Processamento assíncrono
-// A response vai para o primeiro then
-// fetch(url)
-
-//     // Tranformar body em json
-//     .then((response) => response.json())
-
-//     // Pegar results, que é a lista de pokemon
-//     .then(jsonBody => jsonBody.results)
-
-//     // Imprimir o jason
-//     .then((pokemonList) => {
-        
-//         for (let i = 0; i < pokemonList.length; i++) {
-//             const pokemon = pokemonList[i]
-//             console.log(convertPokemonToLi(pokemon))
-//         }
-
-//         document.getElementsByClassName('pokemons')
-
-//     })
-
-//     // Verificar se há erros
-//     .catch ((error) => console.log(error))
-
-//     // Executar quando terminar
-//     // .finally (function(){
-//     //     console.log('Requisição concluída!')
-//     // })
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {       
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i]
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
+    })
+    .catch ((error) => console.log(error))
 
 
 
